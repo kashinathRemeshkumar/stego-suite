97cfe1ae471e9d8106b5b1d4d9d4cc48ada050cb
Kashinath <kashinathremeshkumar@gmail.com>
2026-08-09 12:13:37 +0530

    Audio steg is now working

---
+++ images/decoder.cpp
@@ -0,0 +1,150 @@
+#include<iostream>
+#include<vector>
+#include<fstream>
+#include<string>
+#include<stdexcept>
+#include <filesystem>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+
+struct Header {
+    bool     valid=false;          // did magic bytes match?
+    uint32_t file_size=0;      // size of secret file
+    uint16_t filename_size=0;  // length of filename
+    std::string filename="";    // the actual filename
+    int index=0;               // the current index of the pointer of the image
+};
+
+
+uint8_t read_byte(const uint8_t* image, int &index) {
+    uint8_t buffer = 0;
+
+    for (int j = 7; j >= 0; j--) {
+        uint8_t bit = image[index] & 1;   // extract LSB
+        buffer = buffer | (bit << j);     // place at correct position
+        index++;
+    }
+    return buffer;
+}
+
+
+
+Header get_head_data(const uint8_t* image){
+
+    struct Header header;
+    header.index=0;  //master index of the image
+    std::string magic_byte="";
+
+    for(int i=0;i<4;i++){
+        uint8_t buffer=read_byte(image,header.index);
+        magic_byte=magic_byte+char(buffer);
+    }
+    
+    if (magic_byte[0]=='S' && magic_byte[1]=='T' && magic_byte[2]=='E' && magic_byte[3]=='G'){
+        header.valid=true;
+    }
+
+    if(header.valid){
+        //now get the file size 32 bits
+        uint32_t file_size=0;
+        for(int i=3;i>=0;i--){
+            uint8_t buffer=read_byte(image,header.index);
+            file_size=file_size | (buffer<<i*8);
+        }
+        header.file_size=file_size;
+
+        //filename size
+        uint16_t filename_size = 0;
+        filename_size = read_byte(image, header.index) << 8;
+        filename_size = filename_size | read_byte(image, header.index);
+        header.filename_size = filename_size;
+
+        //actual file name
+        for(int i=0;i<header.filename_size;i++){
+            header.filename+=char(read_byte(image,header.index));
+        }    
+    }
+
+    else{
+        header.valid=false;
+    }
+
+    return header;
+
+}
+
+
+void extract_secret(const uint8_t* image,int height,int width){
+
+    struct Header header=get_head_data(image);
+    std::vector<uint8_t> secret;
+
+    if(header.valid){
+
+        std::cout<<"extracting file \n";
+        std::cout<<"file name: "<<header.filename <<"\n";
+        std::cout<<"file size(bytes) "<<header.file_size<<"\n";
+
+        for(int i = 0; i < (int)header.file_size; i++) {
+            secret.push_back(read_byte(image, header.index));
+        }
+        std::ofstream out(header.filename, std::ios::binary);
+        if (!out) throw std::runtime_error("cannot write: " + header.filename);
+        out.write(reinterpret_cast<const char*>(secret.data()), secret.size());
+
+        std::cout << "Saved to " << header.filename << "\n";
+    }
+    else{
+        throw std::runtime_error("no hidden data found — magic bytes don't match");
+    }
+
+
+
+}
+
+
+
+uint8_t* get_source_image(const std::string& path,int& width,int& height,int& channel){
+    //this function reads the input image and return the bites as a vector
+
+    uint8_t* image=stbi_load(path.c_str(),&width,&height,&channel,3);
+
+    if(image==nullptr){
+        throw std::runtime_error("cannot open file "+ path);
+    }
+    return(image); //
+}
+
+
+int main(int argc, char* argv[]) {
+    // argc = number of arguments including program name
+    // argv[0] = program name
+    // argv[1] = first argument
+
+
+    if (argc < 2) {
+        std::cerr << "Usage: ./decoder path/to/image.png \n";
+        return 1;
+    }
+
+    std::string image_path  = argv[1];
+
+    int width,height,channel;
+    try{
+        uint8_t* image=get_source_image(image_path,width,height,channel); //the source file it is a 1d array of rgbrgbrgb
+
+
+        std::cout << "Image : " << width << "x" << height << "\n";
+
+        extract_secret(image,height,width);
+
+        stbi_image_free(image);
+
+    }
+    catch(std::runtime_error& e){
+        std::cerr<<"ERROR " <<e.what()<< "\n";
+
+    }
+}
--- images/encoder.cpp
+++ images/encoder.cpp
@@ -3,25 +3,72 @@
 #include<fstream>
 #include<string>
 #include<stdexcept>
+#include <filesystem>
 
 #define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 #include "stb_image.h"
 
 
-void embed_secret(const std::vector<uint8_t>& secret, uint8_t* image,int height,int width){
+std::vector<uint8_t> build_header(const std::string& filename, uint32_t file_size){
 
-    int secret_size=secret.size();
-    size_t image_size = width * height * 3;
+    std::vector<uint8_t> header;
 
-    if(image_size<=secret_size*8){
-        throw std::runtime_error("secret file is too large to embed");
+    //magic bytes 4bytes
+    header.push_back('S');
+    header.push_back('T');
+    header.push_back('E');
+    header.push_back('G');
+
+    //file size 4 bytes
+    for (int i = 3; i >= 0; i--) {
+        header.push_back((file_size >> (i * 8)) & 0xFF); //push the file size its 32  bits long
     }
 
-    for (uint8_t byte : secret){
-        for (int i = 7; i >= 0; i--){
-            std::cout << ((byte >> i) & 1);
-        }
-        std::cout << '\n';
+    //file name size 2 bytes
+    uint16_t filename_size=filename.size();
+    header.push_back((filename_size >> 8) & 0xFF); //push the high 8 bits
+    header.push_back((filename_size & 0xFF)); //push the lower 8 bits
+
+    //next is filename variable bytes
+    for (char c : filename) {
+        header.push_back((uint8_t)c); //push the actual file name to header
+    }
+
+    //minimum header size is 15 bytes when file name is 1 char with no extention
+    return header;
+
+}
+
+
+
+void embed_secret(const std::vector<uint8_t>& secret,const std::string& secret_path , uint8_t* image,int height,int width){
+
+    int index=0;
+    size_t image_size = width * height * 3;
+
+    std::vector<uint8_t> header=build_header(std::filesystem::path(secret_path).filename().string(),secret.size());
+
+    std::cout<<" max bytes that can be stored is "<<image_size/8;
+
+    std::vector<uint8_t> stream;
+    stream.insert(stream.begin(),header.begin(),header.end()); //copy header to stream
+    stream.insert(stream.end(),secret.begin(),secret.end());  //copy secret to stream 
+
+
+
+    if(image_size<stream.size()*8){
+        throw std::runtime_error("secret file is too large to embed on to target image use smaller secret file or larger image");
+    }
+    
+    for (uint8_t byte : stream){ //loop to get each byte
+        for (int i = 7; i >= 0; i--){ 
+        
+            image[index]=(image[index] & 0b11111110) | ((byte >> i) & 1);  //reset lsb of the image channel and then set it to the bit extracted from the secret file
+            index++;
+            
+        }   
     }
 }
 
@@ -38,7 +85,7 @@ std::vector<uint8_t> get_secret_file(const std::string& path){
     return std::vector<uint8_t>(
         std::istreambuf_iterator<char>(file),//begining of the file
         std::istreambuf_iterator<char>()//end of the file
-    );
+    ); //returns a vector[array] of bytes of the file 
 }
 
 uint8_t* get_source_image(const std::string& path,int& width,int& height,int& channel){
@@ -49,21 +96,40 @@ uint8_t* get_source_image(const std::string& path,int& width,int& height,int& ch
     if(image==nullptr){
         throw std::runtime_error("cannot open file "+ path);
     }
-    return(image);
+    return(image); //
 }
 
 
-int main(){
+int main(int argc, char* argv[]) {
+    // argc = number of arguments including program name
+    // argv[0] = program name
+    // argv[1] = first argument
+    // argv[2] = second argument
+
+    if (argc < 3) {
+        std::cerr << "Usage: ./encoder path/to/image.png path/to/secret.txt\n";
+        return 1;
+    }
+
+    std::string image_path  = argv[1];
+    std::string secret_path = argv[2];
+
     int width,height,channel;
     try{
-        uint8_t* image=get_source_image("test.png",width,height,channel); //the source file
-        std::vector<uint8_t> secret = get_secret_file("secret.txt"); //the secret file
+        uint8_t* image=get_source_image(image_path,width,height,channel); //the source file it is a 1d array of rgbrgbrgb
+        std::vector<uint8_t> secret = get_secret_file(secret_path); //the secret file
 
 
         std::cout << "Image : " << width << "x" << height << "\n";
         std::cout<<"Size "<<secret.size()<<" Bytes";
 
-        embed_secret(secret,image,height,width);
+        embed_secret(secret,secret_path,image,height,width);
+
+        int ok = stbi_write_png("output.png", width, height, 3, image, width * 3); //save img
+        if (ok == 0) {
+            throw std::runtime_error("failed to save output.png");
+        }
+        std::cout << "\nSaved to output.png\n";
 
         stbi_image_free(image);
 
